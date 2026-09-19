ed615683e3bf488e264895c59708c180188dd690
Tiasha Maitra <146548620+tiashamaitra@users.noreply.github.com>
2026-09-19 09:45:02 +0530

    Time: 2 ms (5.01%), Space: 8.5 MB (53.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp)
-    {
-        if(n==0 || n==1)
-        {
-            return 1;
-        }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        dp[n]=solve(n-1,dp)+solve(n-2,dp);
-        return dp[n];
-    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
-        
+        vector<int>dp(n+1,0);
+        int i;
+        dp[1]=1; //dp[i]= no of ways to reach the ith floor from start
+        dp[0]=1;
+        for(i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+            
+        }
+        return dp[n];
     }
 };

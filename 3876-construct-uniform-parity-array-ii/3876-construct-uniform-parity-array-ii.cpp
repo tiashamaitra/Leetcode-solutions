b28b2eadf7f0cb86ad5308869ad6be80590afd0e
Tiasha Maitra <146548620+tiashamaitra@users.noreply.github.com>
2026-09-27 00:20:30 +0530

    Time: 0 ms (100%), Space: 165.9 MB (20.92%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int smallestOdd = INT_MAX;
+        int smallestEven = INT_MAX;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1[i] % 2 != 0) {
+                smallestOdd = min(smallestOdd, nums1[i]);
+            }
+            else {
+                smallestEven = min(smallestEven, nums1[i]);
+            }
+        }
+
+        // Already all odd
+        if (smallestEven == INT_MAX) {
+            return true;
+        }
+
+        // Already all even
+        if (smallestOdd == INT_MAX) {
+            return true;
+        }
+
+        // Mixed parity: make every even number odd
+        return smallestOdd < smallestEven;
+    }
+};
